ed00db7d835ba68923f52d6a6659bdaca0a679ef
MyrellaMonteiro <Myh.monteiro123@gmail.com>
2026-04-23 21:20:07 -0300

    Add files via upload

---
+++ Lista2exc1_Myrella/Lista2exc1_Myrella/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2exc1_Myrella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.WriteLine("Indique o primeiro valor: ");
+                a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Indique o segundo valor: ");
+                b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+
+            {
+                Console.WriteLine("O primeiro valor é maior");
+            }
+
+            else
+
+            {
+                Console.WriteLine("O segundo valor é maior");
+            }
+
+        }
+    }
+}
