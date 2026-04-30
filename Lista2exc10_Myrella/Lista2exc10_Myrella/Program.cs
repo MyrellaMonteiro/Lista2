24d8398a5d1d38f0fb8936c3a5507d224260608e
MyrellaMonteiro <Myh.monteiro123@gmail.com>
2026-04-30 15:44:37 -0300

    Add files via upload

---
+++ Lista2exc10_Myrella/Lista2exc10_Myrella/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2exc10_Myrella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double P1;
+            double P2;
+            double m;
+
+            Console.WriteLine("Digite a nota da P1:");
+            P1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota da P2:");
+            P2 = double.Parse(Console.ReadLine());
+
+            m = (P1 + 2 * P2) / 3;
+
+            Console.WriteLine("Média: " + m);
+
+            if (m >= 5)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+        }
+    }
+}
---
+++ Lista2exc11_Myrella/Lista2exc11_Myrella/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2exc11_Myrella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double P1;
+            double P2;
+
+            Console.WriteLine("Digite a nota da P1:");
+            P1 = double.Parse(Console.ReadLine());
+
+            P2 = (15 - P1) / 2;
+
+            Console.WriteLine("Você precisa tirar: " + P2 + " na P2 para ser aprovado");
+
+            if (P2 > 10)
+            {
+                Console.WriteLine("Situação: impossível atingir a média");
+            }
+            else if (P2 <= 0)
+            {
+                Console.WriteLine("Situação: já está aprovado");
+            }
+        }
+    }
+}
