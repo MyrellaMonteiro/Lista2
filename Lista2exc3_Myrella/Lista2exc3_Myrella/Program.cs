862113daedd292280b1bc592a62a072654cb4958
MyrellaMonteiro <Myh.monteiro123@gmail.com>
2026-04-30 13:39:48 -0300

    Add files via upload

---
+++ Lista2exc3_Myrella/Lista2exc3_Myrella/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2exc3_Myrella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b;
+            int c;
+            while (a != 69)
+            {
+                Console.WriteLine("Indique o primeiro valor: ");
+                a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Indique o segundo valor: ");
+                b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Indique o terceiro valor: ");
+                c = int.Parse(Console.ReadLine());
+
+                if (a == b && b == c)
+                {
+                    Console.WriteLine("Todos os valores são iguais");
+                }
+                else if (a == b && a > c)
+                {
+                    Console.WriteLine("O 1º e o 2º valor são maiores");
+                }
+                else if (a == c && a > b)
+                {
+                    Console.WriteLine("O 1º e o 3º valor são maiores");
+                }
+                else if (b == c && b > a)
+                {
+                    Console.WriteLine("O 2º e o 3º valor são maiores");
+                }
+                else if (a > b && a > c)
+                {
+                    Console.WriteLine("O 1º valor é maior");
+                }
+                else if (b > a && b > c)
+                {
+                    Console.WriteLine("O 2º valor é maior");
+                }
+                else
+                {
+                    Console.WriteLine("O 3º valor é maior");
+                }
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
---
+++ Lista2exc4_Myrella/Lista2exc4_Myrella/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2exc4_Myrella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int h;
+
+            Console.WriteLine("Indique o valor da base: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Indique o valor da altura: ");
+            h = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A area do retângulo é ");
+            a = b * h;
+            Console.WriteLine(a);
+        
+            if (a >= 100) 
+            {
+                Console.WriteLine("Terreno grande");
+            }
+            else if ( a < 100 && a >= 50)
+            {
+                Console.WriteLine("Terreno médio");
+            }
+            else
+            {
+                Console.WriteLine("Terreno pequeno");
+            }
+        }
+    }
+}
